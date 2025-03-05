6d35378621a015ea04ad7197086f40fe3ecc4e8e
Roman Edirisinghe <rediris@users.noreply.github.com>
2025-03-05 11:38:55 -0500

    fix: add package data
    via @amirtds

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='gymnasium_reporting',
-    version='0.0.1',
+    version='0.0.2',
     license='MIT',
     description='Reporting and data retrieval for Open edX',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'gymnasium_reporting': ['templates/*.html', 'templates/**/*.html'],
+    },
     entry_points={
         'lms.djangoapp': [
             'gymnasium_reporting = gymnasium_reporting.apps:GymnasiumReportingConfig',
